d64d871db22eb0514964bc4b2e59bb06c089c6ad
Małgorzata Skrudlik <sophie.gosia@gmail.com>
2018-06-06 20:30:26 +0200

    Add fix jquery syntax and add event on enter

--- js/script.js
+++ js/script.js
@@ -1,14 +1,24 @@
 const url = 'https://restcountries.eu/rest/v2/name/';
-const countriesList = $('#countries');
+const $countriesList = $('#countries');
 
 $('#search').click(function() {
-
   searchCountries();
+  scroll();
+});
 
+$('html, body').keydown(function(event) {
+  if (event.which == 13) {
+    event.preventDefault();
+    searchCountries();
+    scroll();
+  }
+});
+
+function scroll() {
   $('html, body').animate({
     scrollTop: $('#countries').offset().top
   }, 500);
-});
+}
 
 function searchCountries() {
   let countryName = $('#country-name')
@@ -25,19 +35,22 @@ function searchCountries() {
 }
 
 function showErrorMessage() {
-  countriesList.empty();
-  countriesList.append('<li>Are you sure this is a country name?</li>');
+  $countriesList.empty();
+  $countriesList.append('<li>Are you sure this is a country name?</li>');
 }
 
 function showCountriesList(resp) {
-  countriesList.empty();
+  let $wrapper = $('<div>');
+  $countriesList.empty();
+
   resp.forEach(function(item) {
-    $('<img class="flag">').attr('src', item.flag).appendTo(countriesList);
-    $('<h3>').text(`${item.name} (${item.alpha2Code})`).appendTo(countriesList);
-    $('<p>').text(`Country native name: ${item.nativeName}`).appendTo(countriesList);
-    $('<p>').text(`Capital city: ${item.capital}`).appendTo(countriesList);
-    $('<p>').text(`Subregion: ${item.region}`).appendTo(countriesList);
-    $('<p>').text(`Population: ${item.population}`).appendTo(countriesList);
-    $('<p>').text(`Area (in sqare km): ${item.area}`).appendTo(countriesList);
+    $('<img class="flag">').attr('src', item.flag).appendTo($wrapper);
+    $('<h3>').text(`${item.name} (${item.alpha2Code})`).appendTo($wrapper);
+    $('<p>').text(`Country native name: ${item.nativeName}`).appendTo($wrapper);
+    $('<p>').text(`Capital city: ${item.capital}`).appendTo($wrapper);
+    $('<p>').text(`Subregion: ${item.region}`).appendTo($wrapper);
+    $('<p>').text(`Population: ${item.population}`).appendTo($wrapper);
+    $('<p>').text(`Area (in sqare km): ${item.area}`).appendTo($wrapper);
+    $wrapper.appendTo($countriesList);
   });
 }
